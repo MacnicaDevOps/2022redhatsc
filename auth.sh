1ada07b163f5cf652a10488f534ac25c88369b12
minepicco <minepicco@gmail.com>
2022-10-12 19:36:36 +0900

    try 01

--- auth.sh
+++ auth.sh
@@ -3,4 +3,4 @@
 echo $key>key.json
 gcloud auth activate-service-account --key-file=key.json
 gcloud beta container clusters get-credentials $cluster --region $region --project $project
-alias oc=kubectl
+alias oc="kubectl"
