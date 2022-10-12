00c145286136aac4c33182a9620623d8799c0a4d
minepicco <minepicco@gmail.com>
2022-10-12 19:25:10 +0900

    fix bug in auth.sh

--- auth.sh
+++ auth.sh
@@ -2,5 +2,5 @@
 
 echo $key>key.json
 gcloud auth activate-service-account --key-file=key.json
-gcloud beta container clusters get-credentials $cluster --region $region --project $CIRCLE_PROJECT_REPONAME
+gcloud beta container clusters get-credentials $cluster --region $region --project $project
 alias oc=kubectl
