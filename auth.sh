e1754f315698d453cdaa793b78fc6451f7d47fb2
minepicco <minepicco@gmail.com>
2022-10-17 10:55:14 +0900

    test01

--- auth.sh
+++ auth.sh
@@ -3,6 +3,3 @@
 echo $key>key.json
 gcloud auth activate-service-account --key-file=key.json
 gcloud beta container clusters get-credentials $cluster --region $region --project $project
-echo 'alias oc="kubectl"' >> ~/.bashrc
-echo 'source ~/.bashrc' >> ~/.bash_profile
-source ~/.bashrc
